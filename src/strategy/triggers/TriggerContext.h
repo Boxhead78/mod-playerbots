92c529f189d944303c20be4b5d340489a038f3da
Boxhead78 <maik_baumann90@web.de>
2025-05-07 10:46:56 +0200

    Bots leave the LFG queue less frequently
    so that more bots are in LFR

--- src/strategy/triggers/TriggerContext.h
+++ src/strategy/triggers/TriggerContext.h
@@ -325,7 +325,7 @@ private:
     static Trigger* often(PlayerbotAI* botAI) { return new RandomTrigger(botAI, "often", 5); }
     static Trigger* veryOften(PlayerbotAI* botAI) { return new RandomTrigger(botAI, "very often", 1); }
     static Trigger* RandomLFG(PlayerbotAI* botAI) { return new RandomTrigger(botAI, "random lfg", 150); }
-    static Trigger* RandomLFGLeave(PlayerbotAI* botAI) { return new RandomTrigger(botAI, "random lfg leave", 750); }
+    static Trigger* RandomLFGLeave(PlayerbotAI* botAI) { return new RandomTrigger(botAI, "random lfg leave", 1200); }
     static Trigger* RandomChat(PlayerbotAI* botAI) { return new RandomTrigger(botAI, "random chat", 1500); }
     static Trigger* EnemyOutOfMelee(PlayerbotAI* botAI) { return new EnemyOutOfMeleeTrigger(botAI); }
     static Trigger* EnemyOutOfSpell(PlayerbotAI* botAI) { return new EnemyOutOfSpellRangeTrigger(botAI); }
